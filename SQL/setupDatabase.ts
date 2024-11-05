d797526f09605b7e7be5fb18b02175c6a88cdb14
Gregorio <gregoriocarranza@hotmail.com>
2024-11-05 16:47:56 -0300

    swaggerDocs

--- SQL/setupDatabase.ts
+++ SQL/setupDatabase.ts
@@ -17,13 +17,14 @@ const knexInstance = new KnexConnection().knex;
         table.string('email').unique();
         table.string('nickname').defaultTo('Anonymous');
         table.string('profile_image').defaultTo(null);
-        // .defaultTo(`http://localhost:${process.env.PORT}/profileImgs/defaultProfileImage.jpg);||||
+        // .defaultTo(`http://localhost:${process.env.PORT}/profileImgs/defaultProfileImage.jpg);
         table.string('cover_image').defaultTo(null);
         // .defaultTo(`http://localhost:${process.env.PORT}/profileImgs/defaultCoverImage.jpg`);
         table.text('description');
         table.string('gender');
         table.integer('gamification_level').defaultTo(1);
         table.boolean('active').defaultTo(true);
+        table.string('auth0_id');
         table.timestamp('created_at').defaultTo(knexInstance.fn.now());
         table.timestamp('updated_at').defaultTo(knexInstance.fn.now());
       });
@@ -34,31 +35,31 @@ const knexInstance = new KnexConnection().knex;
 
     //----------- Tabla de seguimiento  -----------
 
-    tableExists = await knexInstance.schema.hasTable('following/followers');
-    if (!tableExists) {
-      await knexInstance.schema.createTable('following/followers', (table) => {
-        table.increments('id').primary();
-        table
-          .string('from')
-          .unsigned()
-          .references('user_uuid')
-          .inTable('user')
-          .onDelete('CASCADE')
-          .withKeyName('fk_from_user');
-        table
-          .string('to')
-          .unsigned()
-          .references('user_uuid')
-          .inTable('user')
-          .onDelete('CASCADE')
-          .withKeyName('fk_to_user');
-        table.timestamp('created_at').defaultTo(knexInstance.fn.now());
-        table.timestamp('updated_at').defaultTo(knexInstance.fn.now());
-      });
-      console.log('following/followers table created successfully.');
-    } else {
-      console.log('Table following/followers already exists.');
-    }
+    // tableExists = await knexInstance.schema.hasTable('following/followers');
+    // if (!tableExists) {
+    //   await knexInstance.schema.createTable('following/followers', (table) => {
+    //     table.increments('id').primary();
+    //     table
+    //       .string('from')
+    //       .unsigned()
+    //       .references('user_uuid')
+    //       .inTable('user')
+    //       .onDelete('CASCADE')
+    //       .withKeyName('fk_from_user');
+    //     table
+    //       .string('to')
+    //       .unsigned()
+    //       .references('user_uuid')
+    //       .inTable('user')
+    //       .onDelete('CASCADE')
+    //       .withKeyName('fk_to_user');
+    //     table.timestamp('created_at').defaultTo(knexInstance.fn.now());
+    //     table.timestamp('updated_at').defaultTo(knexInstance.fn.now());
+    //   });
+    //   console.log('following/followers table created successfully.');
+    // } else {
+    //   console.log('Table following/followers already exists.');
+    // }
   } catch (error) {
     console.error('Error managing the tables:', error);
   } finally {
--- src/app.ts
+++ src/app.ts
@@ -7,9 +7,18 @@ import morgan from 'morgan';
 import path from 'path';
 import { HealthRouter } from './Routes/health/health.routes';
 
+import swaggerUi from 'swagger-ui-express';
+import fs from 'fs';
+import yaml from 'js-yaml';
+import { AuthRouter } from './Routes/Auth.routes';
+
 dotenv.config();
 
 const app: Application = express();
+
+const swaggerDocument = yaml.load(
+  fs.readFileSync('./docs/api-documentation.yaml', 'utf8')
+) as object;
 
 app.use(
   cors({
@@ -25,11 +34,13 @@ app.use(express.urlencoded({ extended: true }));
 
 if (process.env.ENVIRONMENT != 'production') {
   app.use(morgan('tiny'));
+  app.use('/api/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 }
 
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/api/health', new HealthRouter().router);
+app.use('/api/auth', new AuthRouter().router);
 
 app.use(errorMiddleware);
 
