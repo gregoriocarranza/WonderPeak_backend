52d9b271b625c4ff8cb8f56ecbcecc6ae72297f1
Agustin Ezequiel Maio <amaio@MacBook-Air-de-Agustin.local>
2024-12-05 12:44:36 -0300

    [FIX] createdAt

--- SQL/dao/comment.dao.ts
+++ SQL/dao/comment.dao.ts
@@ -126,7 +126,6 @@ export class CommentDAO implements ICommentDAO<CommentInputDTO, IComment> {
       post_uuid: comment?.postUuid,
       comment_uuid: comment?.commentUuid,
       text: comment?.text,
-      created_at: comment?.createdAt,
     };
   }
 
--- SQL/dto/comment/comment.input.dto.ts
+++ SQL/dto/comment/comment.input.dto.ts
@@ -3,14 +3,14 @@ export class CommentInputDTO {
   postUuid: string | null;
   commentUuid: string | null;
   text: string | null;
-  createdAt: Date | null;
+
 
   constructor(data: any) {
     this.userUuid = data.userUuid;
     this.postUuid = data.postUuid;
     this.commentUuid = data.commentUuid;
     this.text = data.text;
-    this.createdAt = data.createdAt || null;
+
   }
 
   public build(): CommentInputDTO {
