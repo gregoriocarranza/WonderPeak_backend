566bc63f06af549536eb7ecbbe721d4c669431fd
Gregorio <gregoriocarranza@hotmail.com>
2024-12-05 13:17:35 -0300

    change

--- src/utils/gamificationLevels.ts
+++ src/utils/gamificationLevels.ts
@@ -1,5 +1,5 @@
 export const levels = [
-  { level: 1, requiredPosts: 0, requiredComments: 0 },
+  { level: 1, requiredPosts: 1, requiredComments: 0 },
   { level: 2, requiredPosts: 2, requiredComments: 0 },
   { level: 3, requiredPosts: 4, requiredComments: 0 },
   { level: 4, requiredPosts: 4, requiredComments: 4 },
